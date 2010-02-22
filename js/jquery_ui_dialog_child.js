3a6af1ea9388507c002c528a083d7ff102e7c37d
Eugen Mayer <mayer@impressive-media.de>
2010-02-22 15:02:07 +0100

    fixing height calculation and JS syntax error

--- js/jquery_ui_dialog_child.js
+++ js/jquery_ui_dialog_child.js
@@ -1,66 +1,58 @@
 // $Id:$
 
 (function ($) {
-Drupal.jqui_dialogChild = Drupal.jqui_dialogChild || {
-  processed: false
-};
-
-/**
- * Child dialog behavior.
- */
-Drupal.jqui_dialogChild.attach = function(context) {  
-  var self = Drupal.jqui_dialogChild;
-  var settings = Drupal.settings.jqui_dialogChild || {};
+  Drupal.jqui_dialogChild = Drupal.jqui_dialogChild || {
+    processed: false
+  };
   
-  // If we cannot reach the parent window, then we have nothing else todo here.
-  if (!self.isObject(parent.Drupal) || !self.isObject(parent.Drupal.jqui_dialog)) {
-    return;
-  }
-
-  // Shortcuts to parent objects.
-  self.$pWindow = parent.jQuery(parent);
-  self.pJQui_dialog = parent.Drupal.jqui_dialog;
-
-  // Make sure this behavior is not processed more than once.
-  if (!self.processed) {
-    self.processed = true;
+  /**
+   * Child dialog behavior.
+   */
+  Drupal.jqui_dialogChild.attach = function(context) {  
+    var self = Drupal.jqui_dialogChild;
+    var settings = Drupal.settings.jqui_dialogChild || {};
     
-	Drupal.jqui_dialogChild.recheckSize();
-
-       
-  }
-  else {  
-    //Drupal.jqui_dialogChild.recheckSize();
-  }
-};
-
-Drupal.jqui_dialogChild.recheckSize = function() {
-  self.pJQui_dialog = parent.Drupal.jqui_dialog;
-  // we need this timeout otherwise the DOM might not be loaded fully yet. This would result in wrong
+    // If we cannot reach the parent window, then we have nothing else todo here.
+    if (!self.isObject(parent.Drupal) || !self.isObject(parent.Drupal.jqui_dialog)) {
+      return;
+    }
+  
+    // Shortcuts to parent objects.
+    self.$pWindow = parent.jQuery(parent);
+    self.pJQui_dialog = parent.Drupal.jqui_dialog;
+    
+    // Make sure this behavior is not processed more than once.
+    if (!self.processed) {
+      self.processed = true;      
+      Drupal.jqui_dialogChild.recheckSize();
+    }
+  };
+    
+  Drupal.jqui_dialogChild.recheckSize = function() {
+    self.pJQui_dialog = parent.Drupal.jqui_dialog;
+    // we need this timeout otherwise the DOM might not be loaded fully yet. This would result in wrong
   // calculation of width / height
-
+  
   setTimeout(function() {    
     var height = $('body').outerHeight();
-    self.pJQui_dialog.chilDocumentSize  = {width: $(window).width(), height: height+30}; 
-    self.pJQui_dialog.resize();
-  },100);
+      self.pJQui_dialog.chilDocumentSize  = {width: $(window).width(), height: height+33}; 
+      self.pJQui_dialog.resize();
+    },100);
+  };
     
-  //}
-};
-
-$(document).ready( function() {
-  $('body').ajaxComplete( function() { 
-	Drupal.jqui_dialogChild.recheckSize();
+  /**
+   * Check if the given variable is an object.
+   */
+  Drupal.jqui_dialogChild.isObject = function(something) {
+    return (something !== null && typeof something === 'object');
+    };
+   
+    $(document).ready( function() {
+      $('body').ajaxComplete( function() { 
+      Drupal.jqui_dialogChild.recheckSize();
+    });
   });
-});
-
-/**
- * Check if the given variable is an object.
- */
-Drupal.jqui_dialogChild.isObject = function(something) {
-  return (something !== null && typeof something === 'object');
-};
-
-Drupal.behaviors.jqui_dialogChild = Drupal.jqui_dialogChild.attach;
+  
+  Drupal.behaviors.jqui_dialogChild = Drupal.jqui_dialogChild.attach;
 
 })(jQuery);
