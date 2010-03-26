fd1806ac8e48d9eed0ee87a4f1bf224e4de25e4c
root <root@debian.impressive.media>
2010-03-26 18:11:38 +0100

    page tpl

--- jquery_ui_dialog-page.tpl.php
+++ jquery_ui_dialog-page.tpl.php
@@ -14,7 +14,7 @@
 <?php print $scripts; ?>
 <link href="<?php print base_path().drupal_get_path('module','jquery_ui_dialog')?>/css/jquery_ui_dialog_child.css" media="all" rel="stylesheet" type="text/css"/>
 </head>
-<body id="jqueryui_dialog">
+<body id="jqueryui_dialog" class="clearfix">
 <div class="dialog-page-wrapper">
   <div class="dialog-page-container clear-block">
     <div class="dialog-page-content">
