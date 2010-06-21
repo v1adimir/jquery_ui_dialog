7ba65e1663478c8f0529f83fc1c244616c93f3d9
Eugen Mayer <eugen.mayer@kontextwork.de>
2010-06-21 13:21:21 +0000

    Adding clear-block ( eventhough its in system/default.css ). Fixing child default css file was not inlcuded

--- jquery_ui_dialog-page.tpl.php
+++ jquery_ui_dialog-page.tpl.php
@@ -14,7 +14,7 @@
 <?php print $scripts; ?>
 <link href="<?php print base_path().drupal_get_path('module','jquery_ui_dialog')?>/css/jquery_ui_dialog.child.css" media="all" rel="stylesheet" type="text/css"/>
 </head>
-<body id="jqueryui_dialog" class="clear-block">
+<body id="jquery_ui_dialog" class="clear-block">
 <div class="dialog-page-wrapper">
   <div class="dialog-page-container">
     <div class="dialog-page-content">
