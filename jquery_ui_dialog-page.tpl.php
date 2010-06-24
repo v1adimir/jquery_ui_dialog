7760d1a7c76ad348872629c50f6cbf64ac4ea8d8
root <root@debian.impressive.media>
2010-06-24 20:41:47 +0200

    adding id to html, clearing html background color

--- jquery_ui_dialog-page.tpl.php
+++ jquery_ui_dialog-page.tpl.php
@@ -6,7 +6,7 @@
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
+<html id="jquery_ui_dialog" xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
 <head>
 <?php print $head; ?>
 <title><?php print (!empty($title) ? strip_tags($title) : $head_title); ?></title>
